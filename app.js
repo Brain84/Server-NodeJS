33d4b697db5e110bce8790226718ecf1d19983ea
Pawel Tatar <pawel.tatar.email@gmail.com>
2019-07-20 20:27:56 +0200

    NodeJS

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const app = express();
 
 // Create server on port 3000
 app.listen(3000, () => {
-	console.log('Server is listening at http://localhost:3000');
+  console.log('Server is listening at http://localhost:3000');
 });
 
 // Load http://localhost:3000/home in a browser to see 'Home page'
 app.get('/home', (req, res) => {
-	res.send('Home page');
+  res.send('Home page');
 });
 
 // Load http://localhost:3000/about in a browser to see 'About us'
 app.get('/about', (req, res) => {
-	res.send('About us', );
+  res.send('About us', );
 });
 
 // Load http://localhost:3000/search in a browser to see home page Google
 app.get('/search', (req, res) => {
-	res.redirect('https://google.com');
+  res.redirect('https://google.com');
 });
 
 // Load http://localhost:3000/logo in a browser to see a photo
@@ -34,8 +34,8 @@ app.get('/logo', (req, res) => {
 
 // Load http://localhost:3000/user/logout in a browser to clear cookie
 app.get('/user/logout', (req, res) => {
-	res.clearCookie('visitor_name');
-	res.send('Cookie has been cleared');
+  res.clearCookie('visitor_name');
+  res.send('Cookie has been cleared');
 });
 
 // Instead ':name' you can use any string eg. 'Paul'. Load http://localhost:3000/user/Paul in a browser to create cookie (name of cookie is Paul)
@@ -72,5 +72,5 @@ app.get('/json', (req, res) => {
 
 // Load any another string eg. 'xyz' localhost:3000/xyz in a browser to see 'Page Not found 404'
 app.get('*', (req, res) => {
-	res.send('<h1>Page Not found 404</h1>', 404);
+  res.send('<h1>Page Not found 404</h1>', 404);
 });
